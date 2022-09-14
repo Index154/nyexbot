70cdfb33744ea11cbb6ec003452f88aa5ea04569
Index154 <flfc007@gmail.com>
2022-09-14 15:19:00 +0200

    Set fix

--- commands/set.js
+++ commands/set.js
@@ -56,38 +56,39 @@ module.exports = {
         }
         
         // If the user doesn't have server admin rights, stop the command
-        var member = (message.client.guilds.cache.get(message.guildId)).members.cache.get(user.id);
-        if(!member.permissions.has(Discord.Permissions.FLAGS.MANAGE_GUILD)){
-            message.reply({ content: "\u274C This command can only be used by those with the permission \"Manage Server\"!", allowedMentions: { repliedUser: false }});
-            return;
-        }
-        
-        // Get the server ID for later
-        var serverID = message.guildId;
-        
-        // Save the second argument as the new prefix or save the channel
-        var change_path = "data/configs/" + serverID + "/prefix.txt";
-        if(args[0] == "channel"){
-            change_path = "data/configs/" + serverID + "/channel.txt";
-            args[1] = message.channel.id;
-        }else{
-            if(args[1] === "" || args[1] === null || args[1] === undefined){
-                message.reply({ content: "\u274C Please define a prefix!", allowedMentions: { repliedUser: false }});
+        (message.client.guilds.cache.get(message.guildId)).members.fetch(user.id).then((member) => {
+            if(!member.permissions.has(Discord.Permissions.FLAGS.MANAGE_GUILD)){
+                message.reply({ content: "\u274C This command can only be used by those with the permission \"Manage Server\"!", allowedMentions: { repliedUser: false }});
                 return;
+            }else{
+                // Get the server ID for later
+                var serverID = message.guildId;
+                
+                // Save the second argument as the new prefix or save the channel
+                var change_path = "data/configs/" + serverID + "/prefix.txt";
+                if(args[0] == "channel"){
+                    change_path = "data/configs/" + serverID + "/channel.txt";
+                    args[1] = message.channel.id;
+                }else{
+                    if(args[1] === "" || args[1] === null || args[1] === undefined){
+                        message.reply({ content: "\u274C Please define a prefix!", allowedMentions: { repliedUser: false }});
+                        return;
+                    }
+                }
+                
+                // If there is no folder for the server yet, create the files
+                var guilddir = "data/configs/" + serverID;
+                if(!fs.existsSync(guilddir)){
+                    fs.mkdirSync(guilddir);
+                    lib.saveFile(guilddir + "/prefix.txt", ",");
+                    lib.saveFile(guilddir + "/channel.txt", "Undefined");
+                }
+                
+                // Change the thing
+                message.reply({ content: "The server's " + args[0] + " has successfully been changed to " + args[1], allowedMentions: { repliedUser: false }});
+                lib.saveFile(change_path, args[1]);
             }
-        }
-        
-        // If there is no folder for the server yet, create the files
-        var guilddir = "data/configs/" + serverID;
-        if(!fs.existsSync(guilddir)){
-            fs.mkdirSync(guilddir);
-            lib.saveFile(guilddir + "/prefix.txt", ",");
-            lib.saveFile(guilddir + "/channel.txt", "Undefined");
-        }
-        
-        // Change the thing
-        message.reply({ content: "The server's " + args[0] + " has successfully been changed to " + args[1], allowedMentions: { repliedUser: false }});
-        lib.saveFile(change_path, args[1]);
+        });
         
 	},
 };
