309b971544b22948a35a543d75c3008aa2940765
Index154 <flfc007@gmail.com>
2023-04-02 12:43:19 +0200

    Changed help text and fixed emoji

--- commands/help.js
+++ commands/help.js
@@ -62,7 +62,7 @@ module.exports = {
             // Set the list of categories in correct order and modified display names
             //var uniqueCategories = categories.filter(onlyUnique);
             var uniqueCategories = ["main", "tasks", "userinfo", "items", "info", "misc", "settings", "variety", "minigames", "admin"];
-            var categoryNames = ["\u2757 Main commands", "\uD83D\uDDD2 Recurring tasks", "\uD83D\uDC64 User information", "\uD83D\uDC8E Item interactions", "\u2139 General information", "\uD83D\uDECD Miscellaneous", "\u2699 Settings / Feedback", "\u2753 Unrelated / Fun commands", "\uD83C\uDFAE Other minigames", "\uD83D\uDD27 Admin commands"];
+            var categoryNames = ["\u2757 Main commands", "\uD83D\uDDD2 Recurring tasks", "\uD83D\uDC64 User information", "\uD83D\uDC8E Item interactions", "ℹ️ General information", "\uD83D\uDECD Miscellaneous", "⚙️ Settings / Feedback", "\u2753 Unrelated / Fun commands", "\uD83C\uDFAE Other minigames", "\uD83D\uDD27 Admin commands"];
             
             // Assemble different versions of the same embed for each page
             var categoriesPerPage = [3, 2, 2, 3];
@@ -73,7 +73,7 @@ module.exports = {
                 var embedTemplate = new Discord.MessageEmbed()
                     .setColor('#0099ff')
                     .setTitle("Command list:")
-                    .setDescription("Visit https://indexnight.com/rpg_info.php for in-depth explanations of everything there is to know about the game!\nIf you are interested in directly interacting with the bot's creator or want to know future update plans then join the main server: https://discord.gg/Sz72qan\nIf you have a problem with the bot then please use the command `" + prefix + "submit`!\n\n" + `By the way: You can send \"${prefix}help [command name]\" to get info on a specific command!`);
+                    .setDescription("You can use `" + prefix + "help [command name]` to get further info about a specific command!" + "\n\nIf you are interested in future update plans for the bot then join the main server: https://discord.gg/Sz72qan\nIf you have a problem with the bot then please use the command `" + prefix + "submit`!");
 
                 // Add commands as multiple fields
                 for(i = 0; i < categoriesPerPage[x]; i++){
